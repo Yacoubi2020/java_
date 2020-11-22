513fd45e008507da6d04d48ce9c208907cd38c33
Yacoubi2020 <73942925+Yacoubi2020@users.noreply.github.com>
2020-11-22 11:07:24 +0100

    Add files via upload

---
+++ Permuter_variables.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Permuter_variables {
+
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		System.out.print("Entrez x: ");
+        int x = scanner.nextInt();
+
+        System.out.print("Entrez y: ");
+        int y = scanner.nextInt();
+
+        System.out.println("Avant permutation: ");
+        System.out.println("x : " + x);
+        System.out.println("y : " + y);
+
+        int tmp = x;
+        x = y;
+        y = tmp;
+
+        System.out.println("Après permutation: ");
+        System.out.println("x : " + x);
+        System.out.println("y : " + y);
+
+	}
+
+}
