2be4b5e5eb4e3f26292ab587d94afbeb1bf3d217
Yacoubi2020 <73942925+Yacoubi2020@users.noreply.github.com>
2020-11-30 11:17:58 +0100

    Add files via upload

--- bus_Trafic.java
+++ bus_Trafic.java
@@ -9,6 +9,7 @@ public class bus_Trafic {
 		double[] prix= {3.20,1.80,2.30,4.20,5.00};
 		bus_Trafic.indice_ville(ville_départ);
 		bus_Trafic.prix_trajet(ville_départ,prix,ville_arrivé);
+		bus_Trafic.prix_avantPromo(ville_départ, prix,ville_arrivé);
 		
 	}
 	  static void indice_ville(String[] u) {
@@ -22,10 +23,21 @@ public class bus_Trafic {
 		  
 		  for(int i=0;i<u.length;i++) 
 			  for(int j=0;j<p.length;j++) {
-			      System.out.println ("le prix de trajet de "+u[i]+" à "+p[j]+" est "+o[j]);	  
+			   //   System.out.println ("le prix de trajet de "+u[i]+" à "+p[j]+" est "+o[j]);	  
 			   
 			    }
 			  }
-		  }
+	       static void prix_avantPromo(String[] u,double[] o,String[] p) {
+		 
+	    	    for(int i=0;i<u.length;i++) 
+				  for(int j=1;j<p.length;j++) {
+					  System.out.println ("le prix de trajet de "+u[i]+" à "+p[j]+" aprés ");
+					  
+					  
+				  }
+				  }
+		   
+		   
+	   }
 		  
 	
